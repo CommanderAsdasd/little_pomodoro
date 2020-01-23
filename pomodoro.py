fcd994be7fb55627d005dd9ff5c61431b27953c3
Aleksandr.Tankovskii <aleksandr.tankovskii@kohls.com>
2020-01-23 22:12:20 +0300

    Removed kitchen timer

--- pomodoro.py
+++ pomodoro.py
@@ -36,19 +36,6 @@ class Timer():
         with open("notes.yaml", "a") as notefile:
             yaml.safe_dump(writable_data, notefile, encoding='utf-8', allow_unicode=True, default_flow_style=False)
 
-    # def kitchen(self,time_start, seconds, minutes):
-        
-    #     while True:
-    #         try:
-    #             sys.stdout.write("\r{minutes} Minutes {seconds} Seconds".format(minutes=minutes, seconds=seconds))
-    #             sys.stdout.flush()
-    #             time.sleep(1)
-    #             seconds = int(time.time() - time_start) - minutes * 60
-    #             if seconds >= 60:
-    #                 minutes += 1
-    #                 seconds = 0
-    #         except KeyboardInterrupt, e:
-    #             break
 
     def countdown(self):
         while self.__countdown > 0:
